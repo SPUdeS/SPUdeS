081caf30c629049d3bfcf0da629c81e6673c1ada
xxdroftartsxx <55997794+xxdroftartsxx@users.noreply.github.com>
2021-02-10 14:21:04 -0500

    Setting up for review of buttons

--- Python-UI/templates/js/index.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-    var btn = document.createElement('input');
-    btn.setAttribute('type', 'button');
-    btn.setAttribute('value', 'BUTTON CLICKED');
-    btn.onclick = sf;
-    document.body.appendChild(btn);
-};
