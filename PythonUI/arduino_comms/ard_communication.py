6032236aa96ef3e8cd3b3850327e77feab73b2ac
davidtrepanier <55953232+davidtrepanier@users.noreply.github.com>
2021-03-17 11:37:26 -0400

    Web page app commit error fix

--- PythonUI/arduino_comms/ard_communication.py
+++ PythonUI/arduino_comms/ard_communication.py
@@ -1,73 +1,69 @@
 import pyfirmata
 from time import sleep
 
-class motorControl:
-    """ Class that lets us control the motors with Communication to an Arduino with pyfirmata"""
+board = pyfirmata.Arduino('COM3')  # port = 'COM3'
+print("Communication Successfully started")
+homingAngle = 5
+max = 90
+min = 0
+pin1 = 7
+pin2 = 9
+pin3 = 11
+pin4 = 13
+pin5 = 5
+pin6 = 3
 
-    def __init__(self):
-        self.board = pyfirmata.Arduino('COM3')  # port = 'COM3'
-        self.homingAngle = 5
-        self.pin1 = 7
-        self.pin2 = 8
-        self.pin3 = 11
-        self.pin4 = 12
-        self.pin5 = 5
-        self.pin6 = 3
-        self.max_angle = 180
-        self.min_angle = 0
-        self.setUpMotors()
+def setUpMotors():
+    board.digital[pin1].mode = pyfirmata.SERVO
+    board.digital[pin2].mode = pyfirmata.SERVO
+    board.digital[pin3].mode = pyfirmata.SERVO
+    board.digital[pin4].mode = pyfirmata.SERVO
+    board.digital[pin5].mode = pyfirmata.SERVO
+    board.digital[pin6].mode = pyfirmata.SERVO
 
+# Custom angle to set Servo motor angle
+def setServoAngle(angle):
+    angle_int = int(angle)
+    setUpMotors()
+    board.digital[pin1].write(180 - angle_int)
+    board.digital[pin2].write(angle_int)
+    board.digital[pin3].write(180 - angle_int)
+    board.digital[pin4].write(angle_int)
+    board.digital[pin5].write(180 - angle_int)
+    board.digital[pin6].write(angle_int)
+    sleep(0.015)
 
-    def setUpMotors(self):
-        self.board.digital[self.pin1].mode = pyfirmata.PWM
-        self.board.digital[self.pin2].mode = pyfirmata.PWM
-        self.board.digital[self.pin3].mode = pyfirmata.PWM
-        self.board.digital[self.pin4].mode = pyfirmata.PWM
-        self.board.digital[self.pin5].mode = pyfirmata.PWM
-        self.board.digital[self.pin6].mode = pyfirmata.PWM
+def goToHomePosition():
+    setServoAngle(homingAngle)
+    print("HOMING NOW")
 
-    # Custom angle to set Servo motor angle
-    def setServoAngle(self, angle):
-        #angle_int = int(angle)
-        self.board.analog.write[self.pin1].write(180.0-angle[1])
-        self.board.analog.write[self.pin2].write(angle[2])
-        self.board.analog.write[self.pin3].write(180.0 - angle[3])
-        self.board.analog.write[self.pin4].write(angle[4])
-        self.board.analog.write[self.pin5].write(180.0 - angle[5])
-        self.board.analog.write[self.pin6].write(angle[6])
-        sleep(0.015)
+def goToUpDownPosition():
+    setServoAngle(homingAngle)
+    for i in range(homingAngle, max):
+        setServoAngle(i)
+        sleep(0.5)
+    for j in range(max, min):
+        setServoAngle(j)
+        sleep(0.5)
+    for k in range(min, homingAngle):
+        setServoAngle(k)
+        sleep(0.5)
 
-    def goToHomePosition(self):
-        self.setServoAngle(self.homingAngle)
-        print("HOMING NOW")
+def goUpPosition():
+    setServoAngle(homingAngle)
+    for i in range(homingAngle, max):
+        setServoAngle(i)
+        sleep(0.5)
 
-    def goToUpDownPosition(self):
-        self.setServoAngle(self.homingAngle)
-        for i in range(self.homingAngle, max):
-            self.setServoAngle(i)
-            sleep(0.5)
-        for j in range(max, min):
-            self.setServoAngle(j)
-            sleep(0.5)
-        for k in range(min, self.homingAngle):
-            self.setServoAngle(k)
-            sleep(0.5)
+def goUPandDown(angle):
+    setServoAngle(angle)
+    sleep(0.015)
 
-    def goUpPosition(self):
-        self.setServoAngle(self.homingAngle)
-        for i in range(self.homingAngle, max):
-            self.setServoAngle(i)
-            sleep(0.5)
+### FOR TESTING ONLY ###
+#while True:
+    #angle = input("Type angle")
+    #goToUpDownPosition()
 
-    def goUpAndDown(self, angle):
-        self.setServoAngle(angle)
-        sleep(0.015)
-
-    ### FOR TESTING ONLY ###
-    #while True:
-        #angle = input("Type angle")
-        #goToUpDownPosition()
-
-        #goUP(angle)
-        #setServoAngle(angle)
-        # goToHomePosition()
+    #goUP(angle)
+    #setServoAngle(angle)
+    # goToHomePosition()
