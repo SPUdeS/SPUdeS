1ef65e5e81ad2d7cba99a23e0a6fcd41cdfdf444
xxdroftartsxx <55997794+xxdroftartsxx@users.noreply.github.com>
2021-03-24 14:51:54 -0400

    Removing plot display testing and cleaning up html code

--- PythonUI/web_page_app.py
+++ PythonUI/web_page_app.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template, Response, request
 import cv2
 #from arduino_comms.ard_communication import setServoAngle, goUPandDown, goToHomePosition, goUpPosition
 from Platform.stewartPlatform import stewartPlatform
-import io
-import random
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
 
 app = Flask(__name__)
 
@@ -78,20 +74,6 @@ def plot_to_UI():
         return 0
     return Response(stewartPlatform.plot())
 
-@app.route('/plot.png')
-def plot_png():
-    fig = create_figure()
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    return Response(output.getvalue(), mimetype='image/png')
-
-def create_figure():
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    xs = range(100)
-    ys = [random.randint(1, 50) for x in xs]
-    axis.plot(xs, ys)
-    return fig
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
